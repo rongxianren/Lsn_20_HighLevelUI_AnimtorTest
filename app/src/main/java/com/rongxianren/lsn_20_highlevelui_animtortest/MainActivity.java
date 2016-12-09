4a731b8c7810f41210d4ad4bc0a601e9d69dd188
rongxianren <rongxianren@163.com>
2016-12-09 16:58:52 +0800

    Initial commit

---
+++ app/src/main/java/com/rongxianren/lsn_20_highlevelui_animtortest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rongxianren.lsn_20_highlevelui_animtortest;
+
+import android.animation.ObjectAnimator;
+import android.animation.TypeEvaluator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
